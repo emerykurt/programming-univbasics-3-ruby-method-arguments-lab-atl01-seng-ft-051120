ec19d7c2025c6cc2f653f42a9089e2f1a1c4e50f
Emery K Lumsden II <emerylumsden@gmail.com>
2020-04-15 16:28:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
-def introduction(kesha)
+def introduction (name)
+  puts "Hi, my name is #{name}."
 end
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
